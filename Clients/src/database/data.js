9998ebad5ff324bfb78bc046f9f0e737fc35a573
bakceesay <90792973+bakceesay@users.noreply.github.com>
2023-10-19 23:04:34 +0000

    Add files via upload

---
+++ Clients/src/database/data.js
@@ -0,0 +1,76 @@
+export default [
+    
+    {
+        id: 1,
+        question: "Which of the following statements is not true?",
+         options :  [ 
+            '(a) IT security can provide protection only against logical or technical attacks.',
+            '(b) The process by which the goals of risk management are achieved is known as risk analysis',
+            '(c) Risks to an IT infrastructure are all computer based',
+            '(d) An asset is anything used in a business process or task.'
+        ]
+
+    },
+
+    {
+        id: 2,
+        question: "Which of the following is not an element of the risk analysis process? ",
+         options :  [ 
+            '(a) Analyzing an environment for risks.',
+            '(b) Creating a cost/benefit report for safeguards to present to upper management',
+            '(c) Selecting appropriate safeguards and implementing them',
+            '(d) Evaluating each threat event as to its likelihood of occurring and cost of the resulting damage'
+        ]
+
+    },
+
+    {
+        id: 3,
+        question: "How is single loss expectancy (SLE) calculated?",
+         options :  [ 
+            '(a) Threat + vulnerability',
+            '(b) Asset value ($) * exposure factor',
+            '(c) Annualized rate of occurrence * vulnerability',
+            '(d) Annualized rate of occurrence * asset value * exposure factor'
+        ]
+
+    },
+
+    {
+        id: 4,
+        question: "An attack in which the attacker remotely controls a set of computers stationed at another geographical location and make them attack a target is known as…?",
+         options :  [ 
+            '(a) SYN Flood attack',
+            '(b) Spoofing',
+            '(c) Piggy Backing',
+            '(d) DOS (Denial of Service)',
+            '(e) None'
+        ]
+
+    },
+
+    {
+        id: 5,
+        question: "When a safeguard or a countermeasure is not present or is not sufficient, what remains ?",
+         options :  [ 
+            '(a) Vulnerability',
+            '(b) Risk',
+            '(c) Penetration',
+            '(d) Attack'
+        ]
+
+    },
+
+    {
+        id: 6,
+        question: "5. Which of the following is n ot a valid definition for risk?",
+         options :  [ 
+            '(a) An assessment of probability, possibility, or chance',
+            '(b) Anything that removes a vulnerability or protects against one or more specific threats',
+            '(c) Risk = threat * vulnerability',
+            '(d) Every instance of exposure'
+        ]
+
+    }
+
+]
